57a2097855fba0a21d946f2616380e2a49cb504c
danna <dannaguisellep@gmail.com>
2025-05-05 17:00:22 -0500

    Aprendiz B: funciones división, módulo y potencia

---
+++ operaciones/aprendiz-b.py
@@ -0,0 +1,14 @@
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: División entre cero"
+
+def modulo(a, b):
+    if b != 0:
+        return a % b
+    else:
+        return "Error: Módulo entre cero"
+
+def potencia(a, b):
+    return a ** b
